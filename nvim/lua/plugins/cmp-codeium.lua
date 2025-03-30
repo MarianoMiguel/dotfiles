c7f44dd47d77678d9bce1d58203f49d6dca431d4
Mariano Miguel <mariano@mariano.io>
2025-03-30 16:42:24 -0300

    updates

--- nvim/lua/plugins/cmp-codeium.lua
+++ nvim/lua/plugins/cmp-codeium.lua
@@ -7,7 +7,7 @@ return {
   opts = function(_, opts)
     table.insert(opts.sources, {
       name = "codeium",
-      group_index = 1, -- Optional: higher priority
+      group_index = 0, -- Optional: higher priority
     })
   end,
 }
