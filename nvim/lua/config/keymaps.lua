45fc25ace5dbfd5728d09ca3a76b562d7ff67266
Mariano Miguel <mariano@mariano.io>
2025-04-05 15:39:13 -0300

    keybindings update

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -8,8 +8,10 @@ local opts = { noremap = true, silent = true }
 -- Normal-mode commands
 vim.keymap.set("n", "mj", ":MoveLine 1<CR>", opts)
 vim.keymap.set("n", "mk", ":MoveLine -1<CR>", opts)
+vim.keymap.set("n", "<C+A+Up>", ":MoveLine -1<CR>", opts)
 vim.keymap.set("n", "mh", ":MoveWord -1<CR>", opts)
 vim.keymap.set("n", "ml", ":MoveWord 1<CR>", opts)
+vim.keymap.set("n", "<C+A+Down>", ":MoveLine 1<CR>", opts)
 
 -- Visual-mode commands
 vim.keymap.set("x", "mj", ":MoveBlock 1<CR>", opts)
@@ -18,26 +20,6 @@ vim.keymap.set("v", "mh", ":MoveHBlock -1<CR>", opts)
 vim.keymap.set("v", "ml", ":MoveHBlock 1<CR>", opts)
 
 vim.keymap.set("n", "<C-c>", ":CodeCompanionChat<CR>")
-
--- vim.keymap.set("n", "<C-h>", "^", opts) -- Start of line (non-blank)
--- vim.keymap.set("n", "<C-l>", "g_", opts) -- End of line (non-blank)
---
--- Insert mode
--- vim.keymap.set("i", "<C-h>", "<C-o>^", opts)
--- vim.keymap.set("i", "<C-l>", "<C-o>$", opts)
---
--- -- Visual mode
--- vim.keymap.set("v", "<C-h>", "^", opts)
--- vim.keymap.set("v", "<C-l>", "$", opts)
---
--- local noop = "<nop>"
---
--- for _, mode in ipairs({ "n", "i", "v" }) do
---   vim.keymap.set(mode, "<Left>", noop, opts)
---   vim.keymap.set(mode, "<Right>", noop, opts)
---   vim.keymap.set(mode, "<Up>", noop, opts)
---   vim.keymap.set(mode, "<Down>", noop, opts)
--- end
 
 vim.keymap.set("n", "<A-Right>", "$")
 vim.keymap.set("n", "<A-Left>", "^")
@@ -51,3 +33,7 @@ vim.keymap.set("n", "<A-S-Up>", "vgg", opts)
 vim.keymap.set("n", "<A-S-Down>", "vG", opts)
 vim.keymap.set("v", "<A-S-Up>", "gg", opts)
 vim.keymap.set("v", "<A-S-Down>", "G", opts)
+vim.keymap.set("n", "<A-Up>", "gg", opts)
+vim.keymap.set("n", "<A-Down>", "G", opts)
+vim.keymap.set("i", "<A-Up>", "<C-o>gg", opts)
+vim.keymap.set("i", "<A-Down>", "<C-o>G", opts)
