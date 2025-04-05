a1f0777adce84e117a7ed20cf455d90f0ed74206
Mariano Miguel <mariano@mariano.io>
2025-04-05 17:23:09 -0300

    update config

--- nvim/lua/plugins/visual-multi.lua
+++ nvim/lua/plugins/visual-multi.lua
@@ -2,4 +2,9 @@ return {
   "mg979/vim-visual-multi",
   branch = "master", -- required
   event = "VeryLazy", -- or "BufReadPre" if you prefer
+  init = function()
+    -- Set Vimscript globals using vim.g
+    vim.g.VM_match_case = 1
+    vim.g.VM_regex_patterns = { [[\<%s\>]] }
+  end,
 }
