896c9bc0d9fbf0fffe2ae585bd878fd810b0d8ba
Mariano Miguel <mariano@mariano.io>
2025-04-10 15:22:34 -0300

    updates

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -21,6 +21,61 @@ vim.keymap.set("v", "ml", ":MoveHBlock 1<CR>", opts)
 
 vim.keymap.set("n", "<C-c>", ":CodeCompanionChat<CR>")
 
+vim.cmd([[function! IndentWithI()
+    if len(getline('.')) == 0
+        return "\"_cc"
+    else
+        return "i"
+    endif
+endfunction
+nnoremap <expr> i IndentWithI()]])
+
+-- vim.cmd([[
+-- function! SmartEnter()
+--   let line = getline('.')
+--   let pos = col('.') - 1
+--   let before = strpart(line, 0, pos)
+--   let after  = strpart(line, pos)
+--
+--   if before =~ '>\s*$' && after =~ '^\s*</'
+--     return "\<CR>\<Esc>O\<Esc>==A"
+--   else
+--     return "\<CR>"
+--   endif
+-- endfunction
+--
+-- inoremap <expr> <CR> SmartEnter()
+-- ]])
+
+vim.cmd([[
+function! SmartEnter()
+  " Get the current line and the cursor column (1-based).
+  let line = getline('.')
+  let pos = col('.') - 1
+
+  " Split the line into "before" and "after" the cursor.
+  let before = strpart(line, 0, pos)
+  let after  = strpart(line, pos)
+
+  " Check if before ends with '>' and after starts with '</'.
+  if before =~ '>\s*$' && after =~ '^\s*</'
+    " 1) <CR>      (splits line at cursor in insert mode)
+    " 2) <Esc>     (switch to normal mode, now on the new second line)
+    " 3) O         (open a blank line above)
+    "    (type two literal spaces in that blank line)
+    " 4) <Esc>     (back to normal mode)
+    " 5) A         (go to end of that line, insert mode again)
+    return "\<CR>\<Esc>O  \<Esc>A"
+  else
+    " Otherwise, just return a normal <CR>.
+    return "\<CR>"
+  endif
+endfunction
+
+" Map <CR> in insert mode to call SmartEnter().
+inoremap <expr> <CR> SmartEnter()
+]])
+
 -- vim.keymap.set("n", "<A-Right>", "$")
 -- vim.keymap.set("n", "<A-Left>", "^")
 -- vim.keymap.set("i", "<A-Left>", "<C-o>^")
