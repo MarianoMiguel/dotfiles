409adff2dece93e1c66552dd173243e70e3ea3a2
Mariano Miguel <mariano@mariano.io>
2025-04-13 02:37:30 -0300

    best config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,40 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize Treesitter
 
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
+    autotag = {
+      enable = true,
+    },
     ensure_installed = {
       "lua",
       "vim",
+      "vimdoc",
+      "bash",
+      "json",
+      "yaml",
+      "markdown",
+      "markdown_inline",
+      "html",
+      "css",
+      "typescript",
+      "javascript",
+      "tsx",
+      "php",
+      "python",
+      "rust",
+      "go",
+      "java",
+      "c",
+      "cpp",
+      "haskell",
+      "scala",
+      "swift",
+      "ruby",
+      "vue",
+      "svelte",
+      "dockerfile",
       -- add more arguments for adding more treesitter parsers
     },
   },
