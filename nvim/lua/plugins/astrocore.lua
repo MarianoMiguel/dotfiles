fa6559b9a89cc1a951ceb8ccb1394483926b4a20
Mariano Miguel <mariano@mariano.io>
2025-04-21 01:05:07 -0300

    updates

--- nvim/lua/plugins/astrocore.lua
+++ nvim/lua/plugins/astrocore.lua
@@ -39,6 +39,7 @@ return {
     -- vim options can be configured here
     options = {
       opt = { -- vim.opt.<key>
+        cursorline = true,
         relativenumber = false, -- sets vim.opt.relativenumber
         number = true, -- sets vim.opt.number
         spell = false, -- sets vim.opt.spell
