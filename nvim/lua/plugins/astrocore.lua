42f3bb4ce53705571278fd275549f4cee34d9bf4
Mariano Miguel <mariano@mariano.io>
2025-06-02 17:11:23 +0000

    updates

--- nvim/lua/plugins/astrocore.lua
+++ nvim/lua/plugins/astrocore.lua
@@ -62,6 +62,9 @@ return {
         -- navigate buffer tabs
         ["]b"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
         ["[b"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
+        -- navigate buffer tabs with <Tab> and <S-Tab>
+        ["<Tab>"] = { function() require("astrocore.buffer").nav(1) end, desc = "Next buffer" },
+        ["<S-Tab>"] = { function() require("astrocore.buffer").nav(-1) end, desc = "Previous buffer" },
 
         -- navigate buffer tabs with <Tab> and <S-Tab>
         ["<Tab>"] = { function() require("astrocore.buffer").nav(1) end, desc = "Next buffer" },
