68d1335180f822158e24579bf3a2e61cb3323292
Mariano Miguel <mariano@mariano.io>
2025-08-03 21:25:11 -0300

    cleanup

--- i3/monitors.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# xrandr --output DP-2 --pos 0x0 --rotate normal --dpi 163
-# xrandr --output DP-2 --pos 0x0 --rotate normal --scale 1x1
-xrandr --output DP-2 --dpi 144
-
--- i3/set-wallpaper.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-feh --bg-scale ~/Pictures/wallpaper.jpg 
