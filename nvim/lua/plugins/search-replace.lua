a1031005f97b8da424cde012f6422f1354f9d460
Mariano Miguel <mariano@mariano.io>
2026-05-30 22:46:57 -0300

    Use Snacks scratch and Grug Far

---
+++ nvim/lua/plugins/search-replace.lua
@@ -0,0 +1,10 @@
+return {
+  { "nvim-pack/nvim-spectre", enabled = false },
+  { "windwp/nvim-spectre", enabled = false },
+  {
+    "MagicDuck/grug-far.nvim",
+    opts = {
+      headerMaxWidth = 80,
+    },
+  },
+}
